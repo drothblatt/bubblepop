77978ae3e4238db059534a8b6e49c78c839e4f7d
nspektor <nellie.spektor@gmail.com>
2016-03-04 09:56:44 -0500

    started

---
+++ pop.js
@@ -0,0 +1,29 @@
+//svg connection                                                               
+var pic = document.getElementById("vimage");
+
+
+var intervalID;
+var radius = 0;
+var playing = false;
+
+var click = function(e){
+    moveBubble();
+    if(!playing){
+	playing = true;
+	//restart timer
+	//add 1 to score
+    }
+}
+
+//move bubble
+//timer
+//scoreboard/end screen display
+
+
+//listen for mouse click
+var play = function(e){
+    e.preventDefault();
+    
+
+};
+
